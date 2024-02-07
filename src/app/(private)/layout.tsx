0e7e8131a80c80bee36b08d45883db45d4f9a1f1
NetoVilela <favneto.a.v.n@gmail.com>
2024-02-06 21:17:40 -0300

    fix: ajustando autenticacao com nextauth e session

--- src/app/(private)/layout.tsx
+++ src/app/(private)/layout.tsx
@@ -1,62 +1,21 @@
-'use client';
-import { useState } from 'react';
-import Loader from '@/components/common/Loader';
-import Sidebar from '@/components/Sidebar';
-import Header from '@/components/Header';
-import { useSession } from 'next-auth/react';
+import { SidebarHeader } from '@/components/SidebarHeader';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '../api/auth/[...nextauth]/options';
 import { redirect } from 'next/navigation';
 
 type Props = {
   children: React.ReactNode;
 }
-export default function RootLayout({ children }: Props) {
-  const [sidebarOpen, setSidebarOpen] = useState(true);
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const [loading, setLoading] = useState<boolean>(true);
-  const { data: session } = useSession({
-    required: true,
-    onUnauthenticated() {
-      redirect('/auth/login');
-    },
-  });
+export default async function RootLayout({ children }: Props) {
+  const session = await getServerSession(authOptions);
 
   if(!session){
-    return <></>;
+    redirect("/auth/login");
   }
-
-  if(loading && session){
-    setLoading(false);
-  }
-
-
-  // useEffect(() => {
-  //   setTimeout(() => setLoading(false), 1000);
-  // }, [session]);
 
   return (
-    
-      <div className="dark:bg-boxdark-2 dark:text-bodydark">
-        {loading ? (
-          <Loader />
-        ) : (
-          <div className="flex h-screen overflow-hidden">
-            <Sidebar
-              sidebarOpen={sidebarOpen}
-              setSidebarOpen={setSidebarOpen}
-            />
-            <div className="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
-              <Header
-                sidebarOpen={sidebarOpen}
-                setSidebarOpen={setSidebarOpen}
-              />
-              <main>
-                <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
-                  {children}
-                </div>
-              </main>
-            </div>
-          </div>
-        )}
-      </div>
+    <SidebarHeader>
+      {children}
+    </SidebarHeader>
   );
 }
---
+++ src/components/SidebarHeader/index.tsx
@@ -0,0 +1,34 @@
+"use client";
+import Sidebar from '@/components/Sidebar';
+import Header from '@/components/Header';
+import { useState } from 'react';
+
+interface Props {
+  children: React.ReactNode;
+}
+
+export const SidebarHeader = ({ children }: Props) => {
+  const [sidebarOpen, setSidebarOpen] = useState(true);
+
+  return(
+    <div className="dark:bg-boxdark-2 dark:text-bodydark">
+      <div className="flex h-screen overflow-hidden">
+        <Sidebar
+          sidebarOpen={sidebarOpen}
+          setSidebarOpen={setSidebarOpen}
+        />
+        <div className="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
+          <Header
+            sidebarOpen={sidebarOpen}
+            setSidebarOpen={setSidebarOpen}
+          />
+          <main>
+            <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
+              {children}
+            </div>
+          </main>
+        </div>
+      </div>
+    </div>
+  )
+}
